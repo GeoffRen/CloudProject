83cc8575cca33e391c2676d660a407d7ddfc7f26
Geoff Ren <geoff.ren@vanderbilt.edu>
2017-12-11 16:40:21 -0600

    Made the map bigger and added boundaries. Added more columns and lasers, the lasers all also spin in different ways.

--- Assets/zzzzProject/Scripts/lazerScript2.cs
+++ Assets/zzzzProject/Scripts/lazerScript2.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class lazerScript2 : MonoBehaviour 
+public class lazerScript2 : MonoBehaviour
 {
+	private static Vector3[] rotationAxises = new Vector3[]
+	{
+		Vector3.up, 
+		Vector3.back, 
+		Vector3.down, 
+		Vector3.forward, 
+		Vector3.left,
+		Vector3.right
+	};
+	
 	public Transform startPoint;
 	public Transform endPoint;
 	private LineRenderer lazerLine;
+	private Vector3 midPoint;
+	private float rotationDegrees;
+	private Vector3 rotationAxis;
 
 	void Start () 
 	{
 		lazerLine = GetComponent<LineRenderer> ();
-		lazerLine.startWidth = .2f;	
+		lazerLine.startWidth = .2f;
+		midPoint = (startPoint.position + endPoint.position) / 2f;
+		rotationDegrees = Random.Range(10f, 100f);
+		rotationAxis = rotationAxises[Random.Range(0, rotationAxises.Length - 1)];
 	}
 	
 	void Update () 
 	{
+		startPoint.RotateAround(midPoint, rotationAxis, rotationDegrees * Time.deltaTime);
+		endPoint.RotateAround(midPoint, rotationAxis, rotationDegrees * Time.deltaTime);
 		lazerLine.SetPosition (0, startPoint.position);
 		lazerLine.SetPosition (1, endPoint.position);		
 	}
