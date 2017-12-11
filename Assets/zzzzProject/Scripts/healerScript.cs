22f51dee417c1a3a3e094dc537161d9d9d3a1258
Lily Van Ryzin <lilyvanryzin@Lilys-MacBook-Air.local>
2017-12-11 15:03:22 -0600

    health boosts

---
+++ Assets/zzzzProject/Scripts/healerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag.Equals("Player"))
+		{
+			Destroy (this.gameObject);
+			var healthManager = other.gameObject.GetComponent<HealthManager>();
+			Debug.Log(string.Format("TRIGGERSTAY: {0} Health at: {1}", other.gameObject.name, healthManager.Health));
+			healthManager.takeDamage(-.01f);
+
+		}
+	}
+}
---
+++ Assets/zzzzProject/Scripts/healerSetScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healerSetScript : MonoBehaviour {
+
+	public Transform healer;
+	public int timer = 500; 
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; ++i) 
+		{
+			float x = Random.Range (-30,30);
+			float y = 1;
+			float z = Random.Range(-30,30);
+			Instantiate (healer, new Vector3(x,y,z), Quaternion.identity);
+		}
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer -= 1;
+		if (timer == 0) 
+		{
+			GameObject[] hs = GameObject.FindGameObjectsWithTag ("Healers");
+			foreach (GameObject h in hs) 
+			{
+				Destroy (h.gameObject);
+			}
+			for (int i = 0; i < 5; ++i) {
+				float x = Random.Range (-30, 30);
+				float y = 1;
+				float z = Random.Range (-30, 30);
+				Instantiate (healer, new Vector3 (x, y, z), Quaternion.identity);
+			}
+			timer = 500;
+		}
+
+	}
+}
